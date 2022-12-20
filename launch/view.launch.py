41bda021fd522c25f470158ea49b024c93f00649
Andrew Sharp <andrew.p.sharp@nasa.gov>
2022-12-20 14:21:57 -0600

    Updates and upgrades. Working with the xacro now. No ROS2 control.4

--- launch/view.launch.py
+++ launch/view.launch.py
@@ -49,7 +49,7 @@ def generate_launch_description():
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
-            PathJoinSubstitution([FindPackageShare("robotiq_hande_description"), "urdf", "robotiq_hande.urdf"]),
+            PathJoinSubstitution([FindPackageShare("robotiq_hande_description"), "urdf", "robotiq_hande.xacro"]),
             " ",
             "jaw_xacro:=",
             jaw_type,
